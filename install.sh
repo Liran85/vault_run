880ce056e13bdd8dbf58264183ee256c73b39cc5
Liran Abadi <lirdan57@gmail.com>
2020-12-28 21:42:04 +0200

    install.sh and uninstall.sh were updated

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 
 APP_NAME="vault_run"
 APP_FULL_PATH="/opt/$APP_NAME/"
+domain="vault_runner"
+
+
 # uninstall application
 
 function install_app() {
@@ -22,11 +25,35 @@ function check_path() {
     
 }
 
+function createAppStructure()
+{
+    local lib=/usr/lib/${domain}/
+    local installerlog=/var/log/${domain}/installerlog
+    local applog=/var/log/${domain}/applog
+    local userdata=$HOME/.${domain}/backups
+    local execfile=/usr/local/bin/${domain}
+    sudo mkdir -p $lib
+    sudo chown  root:$USER $lib
+    sudo chmod g+rw $lib
+    sudo mkdir -p $installerlog
+    sudo chown  root:$USER $installerlog
+    sudo chmod  g+rw $installerlog
+    sudo mkdir -p $applog
+    sudo chown  root:$USER $applog
+    sudo chmod  g+rw $applog
+    mkdir -p $userdata
+    sudo mkdir -p $execfile
+    sudo cp -p $APP_FULL_PATH/vault_runner.sh $execfile
+}
+
+
 function main() {
     echo "=== INSTALLER ==="
     install_app
     check_path
-    echo "::$APP_NAME:: installation completed sucessfully"
+    createAppStructure
+    #echo "::$APP_NAME:: installation completed sucessfully"
+    createAppStructure
 }
 
 # MAIN CALLER
